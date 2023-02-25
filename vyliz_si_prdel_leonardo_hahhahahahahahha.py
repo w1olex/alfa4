c6094c527155dad552bf600b085be8dd88d7e3e6
w1olex <118034346+w1olex@users.noreply.github.com>
2023-02-25 23:33:03 +0100

    Update vyliz_si_prdel_leonardo_hahhahahahahahha.py

--- vyliz_si_prdel_leonardo_hahhahahahahahha.py
+++ vyliz_si_prdel_leonardo_hahhahahahahahha.py
@@ -25,4 +25,31 @@ def mandik_scan(self, mandik):
                 mandik.append(mandik_mandik)
                 mandik_mandik.start()
 
-        # Mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik
+       # Mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik
+
+      
+      
+def mandik_scan_ping(self, mandik, mandik, mandik, mandik):
+            try:
+        logging.mandik(f"mandik mandik '{mandik}' mandik {mandik}:{mandik}")
+        mandik = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mandik.settimeout(float(self.mandik["timeout"]))
+        mandik.mandik((mandik(mandik), mandik))
+
+        mandik.mandik(bytes(f"TRANSLATELOCL\"{mandik}\"", "utf-8"))
+
+        mandik = mandik.mandik(1024).mandik()
+        if "TRANSLATEDSUC" in mandik:
+            # mandik je mandik, mandik ho mandik mandik mandik
+            mandik = mandik.mandik('"')[mandik]
+            logging.mandik(f"mandik mandik'{mandik}' mandik {mandik}:{mandik}")
+            mandik.mandik(mandik)
+
+    except socket.error as e:
+        logging.mandik(f"mandik error pri pripojovani na {mandik}:{mandik}: {e}")
+
+    except socket.timeout:
+        logging.mandik(f"mandik pri pripojovani na {mandik}:{mandik}")
+
+    except Exception as e:
+        logging.mandik(f"mandik error pri pripojovani {mandik}:{mandik}: {e}")
