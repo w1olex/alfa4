7fd5c330897bd77c48479f314f75358e99005947
w1olex <118034346+w1olex@users.noreply.github.com>
2023-02-25 23:30:24 +0100

    Update vyliz_si_prdel_leonardo_hahhahahahahahha.py

--- vyliz_si_prdel_leonardo_hahhahahahahahha.py
+++ vyliz_si_prdel_leonardo_hahhahahahahahha.py
@@ -1,1 +1,28 @@
-HAHASHAHHAHAHAHAHAHAHAHHAHAHAHAHHA
+def mandik_scan(self, mandik):
+  try:
+        if '"' in mandik:
+            mandik_mandik = mandik.split("\"")[1]
+            if (mandik_mandik == ""):
+                return "MANDIKMOR\"Mandik mandik mandik mandik\""
+        else:
+            return "MANDIKMOR\"Mandik mandik mandik\""
+
+        # Mandik, mandik, mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik
+        if mandik_mandik in self.mandik_mandik:
+            # Mandik, mandik mandik mandik mandik mandik mandik mandik mandik mandik
+            return "MANDIKSUCC\"" + self.mandik_mandik[mandik_mandik] + "\""
+
+        # Mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik 
+        mandik_mandik_mandik = self.config["mandik_mandik_mandik"]
+        mandik_mandik_mandik_mandik = self.config["mandik_mandik_mandik_mandik"]
+        mandik_mandiks = []
+        mandik = []
+
+        # Mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik 
+        for mandik in mandik_mandik_mandik.hosts():
+            for mandik_mandik_mandik_mandik in mandik_mandik_mandik:
+                mandik_mandik = threading.Thread(target=self.mandik_mandik_mandik, args=(mandik, mandik_mandik_mandik_mandik, mandik_mandik_mandik, mandik_mandiks))
+                mandik.append(mandik_mandik)
+                mandik_mandik.start()
+
+        # Mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik mandik
